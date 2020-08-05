317d52ca7868882292fc4b326482a0462f937cd7
prachibaheti <65976476+prachibaheti@users.noreply.github.com>
2020-08-05 14:02:56 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+                                                                                                                                var ball,img,paddle;
+function preload() {
+ 
+}
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(205,153,0);
+  
+}
+
+
+
